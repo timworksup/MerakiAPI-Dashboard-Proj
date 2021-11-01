509039790fa2ea4939d2a62ce11172994e1dcf5e
Tim Hjort <tim.hjort@cygate.se>
2021-11-01 14:01:36 +0100

    Added a Get Network script into the rep

---
+++ Network_Scripts/DeleteNetwork.py
@@ -0,0 +1,18 @@
+import requests
+
+API_Key = input("Enter your API Key:")
+NetworkID = input("Enter the ID of the network that is wished to be removed: ")
+
+url = "https://api.meraki.com/api/v1/networks/%s" % (NetworkID)
+print(url)
+
+payload = None
+headers = {
+    "Content-Type": "applications/json",
+    "Accept": "applications/json",
+    "X-Cisco-Meraki-API-Key": (API_Key),
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
